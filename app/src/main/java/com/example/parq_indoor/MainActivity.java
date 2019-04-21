a2ff4c4db7f0a5be3a185ebb696a091f2e7eb66e
Miguel Jose Obregon <miguel.j.obregon@gmail.com>
2019-04-21 17:56:37 -0500

    Add BLE scanning functionality

--- app/src/main/java/com/example/parq_indoor/MainActivity.java
+++ app/src/main/java/com/example/parq_indoor/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.parq_indoor;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.ParcelUuid;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import com.example.parq_indoor.Layout.LayoutGraph;
 import com.example.parq_indoor.Layout.LayoutGraphBuilder;
+import com.example.parq_indoor.Layout.ParkingNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
+    public static final ParcelUuid Service_UUID = ParcelUuid
+            .fromString("0000b81d-0000-1000-8000-00805f9b34fb");
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int MY_PERMISSIONS_REQUEST = 1;
+
+    private static boolean permissionGranted = false;
+
     public LayoutGraph layoutGraph;
+
+    private ParkingView parkingView;
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private BluetoothLeScanner mBluetoothLeScanner;
+
+    private ScanCallback mScanCallback;
+
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +55,111 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState == null) {
+            mBluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+            mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+            mHandler = new Handler();
+        }
+
         layoutGraph =
                 LayoutGraphBuilder.buildLayoutGraphFromJson(this, R.raw.demo_layout);
-        ParkingView parkingView = findViewById(R.id.parkingView);
+        parkingView = findViewById(R.id.parkingView);
         parkingView.setLayoutGraph(layoutGraph);
+        getPermission();
+        startScanning();
+    }
+
+    public void getPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST);
+
+        } else {
+            permissionGranted = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * Start scanning for BLE Advertisements (& set it up to stop after a set period of time).
+     */
+    public void startScanning() {
+        if (mScanCallback == null) {
+            // Kick off a new scan.
+            mScanCallback = new ParkingScanCallback();
+            mBluetoothLeScanner.startScan(buildScanFilters(), buildScanSettings(), mScanCallback);
+        }
+    }
+
+    /**
+     * Stop scanning for BLE Advertisements.
+     */
+    public void stopScanning() {
+        mBluetoothLeScanner.stopScan(mScanCallback);
+        mScanCallback = null;
+    }
+
+    /**
+     * Return a List of {@link ScanFilter} objects to filter by Service UUID.
+     */
+    private List<ScanFilter> buildScanFilters() {
+        List<ScanFilter> scanFilters = new ArrayList<>();
+
+        ScanFilter.Builder builder = new ScanFilter.Builder();
+        // Comment out the below line to see all BLE devices around you
+        builder.setDeviceAddress("00:0B:57:31:55:2A");
+        //builder.setServiceUuid(Service_UUID);
+        scanFilters.add(builder.build());
+
+        return scanFilters;
+    }
+
+    /**
+     * Return a {@link ScanSettings} object set to use low power (to preserve battery life).
+     */
+    private ScanSettings buildScanSettings() {
+        ScanSettings.Builder builder = new ScanSettings.Builder();
+        builder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);
+        return builder.build();
+    }
+
+
+    private class ParkingScanCallback extends ScanCallback {
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+
+            byte[] data = result.getScanRecord().getManufacturerSpecificData(767);
+            List<ParkingNode> nodes = layoutGraph.nodes.stream().filter(n -> n instanceof ParkingNode).map(n -> (ParkingNode)n).collect(Collectors.toList());
+            if (data != null) {
+                Log.d(TAG, "Data: " + Arrays.toString(data));
+                for (ParkingNode node : nodes) {
+                    node.isOccupied = true;
+                }
+                for (int x = 0; x < data.length-1; x++) {
+                    nodes.get(x).isOccupied = data[x] == 1;
+                }
+            }
+
+            parkingView.invalidate();
+        }
     }
 }
